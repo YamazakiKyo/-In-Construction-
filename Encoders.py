703d743e53c38183fe821a1703b842ab1cd36e40
Yagami <hyxx123@gmail.com>
2018-05-29 15:37:13 -0500

    V1_encoder

--- Encoders.py
+++ Encoders.py
@@ -1,38 +1,51 @@
 from tqdm import tqdm
 from time import sleep
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, normalize
 
-features = ['ALIGNMENT_CD', 'CRASH_YEAR', 'CR_MONTH', 'DAY_OF_WK', 'DR_SEX',
-            'HIT_AND_RUN', 'LIGHTING_CD', 'LOC_TYPE_CD', 'PEDESTRIAN',
-            'PRI_CONTRIB_FAC_CD', 'ROAD_COND_CD', 'ROAD_TYPE_CD',
-            'SURF_COND_CD', 'SURF_TYPE_CD', 'WEATHER_CD', 'city_level', 'age_group',
-            'crash_time', 'vehicle_type', 'From_Louisiana', 'vehicle_year',
-            'num_occupant', 'num_vehicle', 'aggressive_driving',
-            'drive_condition', 'drive_distract', 'manner_of_collision']
+# features = ['ALIGNMENT_CD', 'CRASH_YEAR', 'CR_MONTH', 'DAY_OF_WK', 'DR_SEX',
+#             'HIT_AND_RUN', 'LIGHTING_CD', 'LOC_TYPE_CD', 'PEDESTRIAN',
+#             'PRI_CONTRIB_FAC_CD', 'SEC_CONTRIB_FAC_CD','ROAD_COND_CD', 'ROAD_TYPE_CD',
+#             'SURF_COND_CD', 'SURF_TYPE_CD', 'WEATHER_CD', 'city_level', 'age_group',
+#             'crash_time', 'vehicle_type', 'From_Louisiana', 'vehicle_year',
+#             'num_occupant', 'num_vehicle', 'aggressive_driving',
+#             'drive_condition', 'drive_distract', 'manner_of_collision']
+features = ['CR_MONTH', 'ALIGNMENT_CD', 'PRI_CONTRIB_FAC_CD',
+       'SEC_CONTRIB_FAC_CD', 'DAY_OF_WK', 'HIT_AND_RUN', 'LIGHTING_CD',
+       'LOC_TYPE_CD', 'NUM_VEH', 'ROAD_COND_CD', 'ROAD_REL_CD', 'ROAD_TYPE_CD',
+       'SURF_COND_CD', 'SURF_TYPE_CD', 'WEATHER_CD',
+       'PEDESTRIAN', 'DR_SEX', 'From_Louisiana', 'NUM_OCC',
+        'city_level', 'highway_type', 'manner_of_collision',
+       'aggressive_driving', 'age_group', 'crash_time', 'vehicle_type',
+       'drive_distract', 'drive_condition', 'vehicle_year']
 
-def encoder(year):
-    df_train = pd.read_csv('./' + str(year) + '_cleaned.csv')
-    X_train = df_train[features]
-    y_train = df_train['SEVERITY_CD']
-    y_train = y_train.fillna('NaN')
-    y_train = LabelEncoder().fit_transform(y_train)
-    int_list = ['CRASH_YEAR', 'CR_MONTH', 'HIT_AND_RUN', 'PEDESTRIAN', 'age_group', 'crash_time', 'aggressive_driving']
-    for col in tqdm(int_list):
-        X_train[str(col)] = X_train[str(col)].dropna().apply(lambda x: int(x))
-        X_train[str(col)]=X_train[str(col)].fillna(-1)
-        X_train[str(col)] = LabelEncoder().fit_transform(X_train[str(col)])
-        sleep(0.1)
 
-    for items in tqdm(X_train.columns):
-        if X_train.dtypes[str(items)] == 'object':
-            X_train[str(items)]=X_train[str(items)].fillna('NaN')
-            X_train[str(items)] = LabelEncoder().fit_transform(X_train[str(items)])
-        sleep(0.1)
+year = 2014
 
-    X_train.to_csv('./data_encoded/'+str(year)+'_data.csv', index=False)
-    y_train_df = pd.DataFrame(y_train)
-    y_train_df.to_csv('./data_encoded/'+str(year)+'_label.csv', index=False)
+df_train = pd.read_csv('./data_cleaned/' + str(year) + '_cleaned.csv')
+X = df_train[features]
+y = df_train['SEVERITY_CD']
+y = y.fillna('NaN')
+y = LabelEncoder().fit_transform(y)
+int_list = ['CR_MONTH', 'HIT_AND_RUN', 'NUM_VEH','PEDESTRIAN', 'age_group',
+            'crash_time', 'NUM_OCC', 'vehicle_year']
+for col in tqdm(int_list):
+    X[str(col)] = X[str(col)].dropna().apply(lambda x: int(x))
+    X[str(col)]=X[str(col)].fillna(-1)
+    # X[str(col)] = LabelEncoder().fit_transform(X[str(col)])
+    X[str(col)] = X[str(col)].astype(int)
+    sleep(0.1)
 
-for yr in range(2006, 2007):
-    encoder(yr)
+obj_list = []
+for items in tqdm(X.columns):
+    if items not in int_list:
+        obj_list.append(items)
+
+for items in tqdm(obj_list):
+    X[str(items)]=X[str(items)].fillna('NaN')
+    X[str(items)] = LabelEncoder().fit_transform(X[str(items)])
+    sleep(0.1)
+
+X.to_csv('./data_encoded/' + str(year) + '_data.csv', index=False)
+y_train_df = pd.DataFrame(y)
+y_train_df.to_csv('./data_encoded/'+str(year)+'_label.csv', index=False)
