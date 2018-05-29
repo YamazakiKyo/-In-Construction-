fff44d86151d4d26a343bc3b8418b2574b06e535
Yagami <hyxx123@gmail.com>
2018-05-29 15:38:58 -0500

    V2_encoder_IMBA

--- Encoders.py
+++ Encoders.py
@@ -1,15 +1,12 @@
 from tqdm import tqdm
 from time import sleep
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder, normalize
+from sklearn.preprocessing import LabelEncoder
+import ictaiDefs.DataInput as iD
 
-# features = ['ALIGNMENT_CD', 'CRASH_YEAR', 'CR_MONTH', 'DAY_OF_WK', 'DR_SEX',
-#             'HIT_AND_RUN', 'LIGHTING_CD', 'LOC_TYPE_CD', 'PEDESTRIAN',
-#             'PRI_CONTRIB_FAC_CD', 'SEC_CONTRIB_FAC_CD','ROAD_COND_CD', 'ROAD_TYPE_CD',
-#             'SURF_COND_CD', 'SURF_TYPE_CD', 'WEATHER_CD', 'city_level', 'age_group',
-#             'crash_time', 'vehicle_type', 'From_Louisiana', 'vehicle_year',
-#             'num_occupant', 'num_vehicle', 'aggressive_driving',
-#             'drive_condition', 'drive_distract', 'manner_of_collision']
+year = 2014
+
+
 features = ['CR_MONTH', 'ALIGNMENT_CD', 'PRI_CONTRIB_FAC_CD',
        'SEC_CONTRIB_FAC_CD', 'DAY_OF_WK', 'HIT_AND_RUN', 'LIGHTING_CD',
        'LOC_TYPE_CD', 'NUM_VEH', 'ROAD_COND_CD', 'ROAD_REL_CD', 'ROAD_TYPE_CD',
@@ -19,20 +16,63 @@ features = ['CR_MONTH', 'ALIGNMENT_CD', 'PRI_CONTRIB_FAC_CD',
        'aggressive_driving', 'age_group', 'crash_time', 'vehicle_type',
        'drive_distract', 'drive_condition', 'vehicle_year']
 
-
-year = 2014
-
-df_train = pd.read_csv('./data_cleaned/' + str(year) + '_cleaned.csv')
-X = df_train[features]
-y = df_train['SEVERITY_CD']
-y = y.fillna('NaN')
-y = LabelEncoder().fit_transform(y)
 int_list = ['CR_MONTH', 'HIT_AND_RUN', 'NUM_VEH','PEDESTRIAN', 'age_group',
             'crash_time', 'NUM_OCC', 'vehicle_year']
+
+df = pd.read_csv('./data_cleaned/' + str(year) + '_cleaned.csv')
+
+
+Min = df.loc[(df['SEVERITY_CD'] == 'A')]
+Middle = df.loc[(df['SEVERITY_CD'] == 'C')| (df['SEVERITY_CD'] == 'B')]
+Maj = df.loc[(df['SEVERITY_CD'] != 'A') & (df['SEVERITY_CD'] != 'B') & (df['SEVERITY_CD'] != 'C')]
+
+
+data_min = Min[features].as_matrix()
+label_min = Min['SEVERITY_CD'].values.reshape(-1, 1)
+data_Middle = Middle[features].as_matrix()
+label_Middle = Middle['SEVERITY_CD'].values.reshape(-1, 1)
+data_maj = Maj[features].as_matrix()
+label_maj = Maj['SEVERITY_CD'].values.reshape(-1, 1)
+
+data_new = np.append(data_min, data_min, axis=0)
+label_new = np.append(label_min, label_min, axis=0)
+for i in range(5):
+    data_new = np.append(data_new, data_min, axis=0)
+    label_new = np.append(label_new, label_min, axis=0)
+
+num_supple = data_Middle.shape[0] - data_new.shape[0]
+for i in range(num_supple):
+    rand_pick = np.random.randint(data_Middle.shape[0]-1)
+    data_new = np.append(data_new, data_Middle[rand_pick, :].reshape(1, -1), axis=0)
+    label_new = np.append(label_new, label_Middle[rand_pick, :].reshape(1, -1), axis=0)
+
+data_new = np.append(data_new, data_maj, axis=0)
+label_new = np.append(label_new, label_maj, axis=0)
+
+print(iD.count_value(df['SEVERITY_CD'].values))
+print(iD.count_value(label_new))
+
+X = pd.DataFrame(data_new, columns=features)
+y = pd.DataFrame(label_new, columns=['SEVERITY_CD']).fillna('NaN')
+y = LabelEncoder().fit_transform(y)
+
+# ambi = data_min[0, :].reshape(1, data_min[0, :].shape[0])
+
+# for i in range(data_min.shape[0]):
+#     print('i', i)
+#     for j in range(data_min.shape[0]):
+#         if j % 10000 == 0:
+#             print('j', j)
+#         if np.array_equal(data_min[i, :], data_maj[j, :]):
+#             ambi = np.append(ambi, data_min[i, :], axis=0)
+#             print(data_min[i, :])
+#
+# print(ambi.shape)
+
 for col in tqdm(int_list):
     X[str(col)] = X[str(col)].dropna().apply(lambda x: int(x))
     X[str(col)]=X[str(col)].fillna(-1)
-    # X[str(col)] = LabelEncoder().fit_transform(X[str(col)])
+    X[str(col)] = LabelEncoder().fit_transform(X[str(col)])
     X[str(col)] = X[str(col)].astype(int)
     sleep(0.1)
 
@@ -46,6 +86,7 @@ for items in tqdm(obj_list):
     X[str(items)] = LabelEncoder().fit_transform(X[str(items)])
     sleep(0.1)
 
-X.to_csv('./data_encoded/' + str(year) + '_data.csv', index=False)
-y_train_df = pd.DataFrame(y)
-y_train_df.to_csv('./data_encoded/'+str(year)+'_label.csv', index=False)
+X.to_csv('./data_encoded/' + str(year) + '_v2_data.csv', index=False)
+y = pd.DataFrame(y)
+y.to_csv('./data_encoded/'+str(year)+'_v2_label.csv', index=False)
+
